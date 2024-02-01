faecd13e104cc2e57ce6848e56e9b7f4a45d3335
Ossy-em <emosinachi@gmail.com>
2024-02-02 00:19:26 +0100

    updates

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../../Components/Assets/app-products.js/logo.png'; 
+import cart_icon from '../../Components/Assets/app-products.js/cart_icon.png';
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <div className='nav-logo'>
+            <img src={logo} alt=''/>
+<p>SHOPPER</p>
+        </div>
+        <ul className='nav-menu'>
+            <li>Shops <hr /> </li>
+            <li>Men</li>
+            <li>Women</li>
+            <li>Kids</li>
+        </ul>
+      <div className='nav-login-cart'>
+        <button>Login</button>
+        <img src={cart_icon} alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
