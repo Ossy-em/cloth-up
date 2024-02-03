18f6b029272d1c1d496be62a0bf64bf5d9142870
Ossy-em <emosinachi@gmail.com>
2024-02-03 23:23:55 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
-
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import ShopCategory from './Pages/ShopCategory';
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
    <Navbar/>
+   <Routes>
+    <Route path='/' element={<Shop/>}/>
+    <Route path='/men' element={<ShopCategory category='men'/>}/>
+    <Route path='/women' element={<ShopCategory category='women'/>}/>
+     <Route path='/kids' element={<Shop/>}/>
+    </Routes>
+   </BrowserRouter>
     </div>
   );
 }
